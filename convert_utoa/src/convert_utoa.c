b2defdb05afc6508a5b34c6cdf4ff3f063146cd3
ismailessaidi11 <91049455+ismailessaidi11@users.noreply.github.com>
2024-10-03 10:49:12 +0200

    convert program functionnal

--- convert_utoa/src/convert_utoa.c
+++ convert_utoa/src/convert_utoa.c
@@ -1,47 +1,10 @@
 #include <stdio.h>
-#include <vga.h>
 #include <string.h>
 #include <spr.h>
 #include <convert_utoa.h>
 
-unsigned int utoa(unsigned int number, char *buf, unsigned int bufsz, unsigned int base, const char *digits) {
-    unsigned int q = 0;
-    unsigned int r = 0;
-    int pos = bufsz - 2; 
-
-    if (!(bufsz > 1 && base > 1)) {
-        /* Failure */
-        buf[0] = '\0';
-        return 0;
-    }
-
-    buf[bufsz - 1] = '\0';  // Null terminate the buffer
-
-    do {
-        if (pos < 0) {
-            // If the buffer is too small, prevent overflow
-            buf[0] = '\0'; 
-            return 0;
-        }
-        q = number / base;
-        r = number % base;
-        buf[pos--] = digits[r];  
-        number = q;  
-    } while (number > 0);
-
-    // Shift the result if it doesn't start at the beginning of the buffer
-    memmove(buf, &buf[pos + 1], bufsz - pos - 1);
-
-    // Return the number of characters written (excluding null terminator)
-    return bufsz - pos - 2;
-}
-
-int main() {
-    int reg;
-
-    // Assuming `vga_clear()` is part of your environment, if not, omit it
-    // vga_clear(); // Uncomment if necessary
-
+int main() 
+{
     printf("\nConverting from base 10 to base 20...\n");
 
     unsigned int base = 20;
@@ -54,8 +17,34 @@ int main() {
         printf("%u = %s\n", i, buf);
     }
 
-    asm volatile ("l.ori %[out1],r1,0" : [out1] "=r" (reg));
-    printf("My stacktop = 0x%08X\n", reg);
-
     return 0;
 }
+
+unsigned int utoa(unsigned int number, char *buf, unsigned int bufsz, unsigned int base, const char *digits) 
+{
+    unsigned int q = 0;
+    unsigned int r = 0;
+    int pos = bufsz - 2; /* Start at the end of the buffer taking into consideration '\0' to be the last character*/
+
+    if (!(bufsz > 1 && base > 1)) /* prevent overflow if buffer too small, checking valid base */
+    {
+        /* Failure */
+        buf[0] = '\0';
+        return 0;
+    }
+
+    buf[bufsz - 1] = '\0';  // Null terminate the buffer
+
+    do {
+        q = number / base;
+        r = number % base;
+        buf[pos--] = digits[r];  
+        number = q;  
+    } while (number > 0);
+
+    // Shift the result if it doesn't start at the beginning of the buffer
+    memmove(buf, &buf[pos + 1], bufsz - pos - 1);
+
+    return bufsz - pos - 2;
+}
+
